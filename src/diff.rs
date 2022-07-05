331ad168821f4018644d2fcba67eda2a41cd1b1b
CreatorSiSo <64036709+CreatorSiSo@users.noreply.github.com>
2022-07-06 00:23:31 +0200

    Add vector splitting for diff_compress.

--- src/diff.rs
+++ src/diff.rs
@@ -1,55 +1,51 @@
-#[derive(Debug, Default)]
+#[derive(Debug, Default, PartialEq, Eq, PartialOrd, Ord)]
 struct Diff {
 	average: u8,
 	diff: Vec<u8>, // TODO: Make this signed
 }
 
 impl Diff {
-	fn new(average: u8, diff: Vec<u8>) -> Self {
+	fn _new(average: u8, diff: Vec<u8>) -> Self {
 		Self { average, diff }
 	}
 }
 
-fn _diff_compress(bytes: Vec<u8>, max_dist: u8) -> Vec<u8> {
-	println!("Bytes: {:?}\n", bytes);
+fn _diff_compress(bytes: Vec<u8>, max_diff: u8) -> Vec<Vec<u8>> {
+	let result = bytes.iter().cloned().enumerate().fold(
+		(vec![vec![]], 0_usize),
+		|(mut sequences, sequence_start), (index, byte)| {
+			let sequence = &bytes[sequence_start..index];
 
-	let result = bytes.into_iter().fold(
-		(0_usize, 0_usize, 0_u8, Vec::default()),
-		|(len, sum, avrg, mut plateaus /*, diffs */), current| {
-			if len < 1 {
-				return (1, current.into(), current, plateaus);
+			if !sequence.is_empty() {
+				println!("\nSequence: {:?}", sequence);
+
+				if let Some(next_byte) = bytes.get(index) {
+					println!("Next Byte: {:?}", next_byte);
+
+					let sum = sequence.iter().fold(0_usize, |acc, v| acc + *v as usize);
+					let avrg = (sum / sequence.len()) as u8;
+					println!("Avrg: {:?}", avrg);
+
+					let next_diff = avrg.abs_diff(*next_byte);
+					let diff_too_big = next_diff > max_diff;
+					println!("Next Diff: {:?} [{}]", next_diff, diff_too_big);
+
+					if diff_too_big {
+						println!("-> Creating new sequence");
+						sequences.push(Vec::new());
+						sequences.last_mut().unwrap().push(byte);
+						return (sequences, index);
+					}
+				}
 			}
 
-			let diff = avrg.abs_diff(current);
-			if diff > max_dist {
-				plateaus.push(Diff::new(avrg, Vec::new()));
-				return (1, current.into(), current, plateaus);
-			}
-
-			let new_avrg = (sum / len) as u8;
-			println!("Avrg: {new_avrg}, Current: {current} Diff:{diff}");
-			// plateaus.last_mut().unwrap().push(new_average);
-			(len + 1, sum + current as usize, new_avrg, plateaus)
-
-			// plateaus.push(average.abs_diff(current.into()));
+			sequences.last_mut().unwrap().push(byte);
+			(sequences, sequence_start)
 		},
 	);
 
-	println!("\n{:?}\n", result);
-
-	// let mut accum: u8 = 0;
-	// let mut len: usize = 0;
-	// for byte in bytes {
-	// 	let average = (accum as usize / len) as u8;
-	// 	if accum.abs_diff(byte) <= max_distance {
-	// 		accum = byte;
-	// 		len = 1;
-	// 	}
-	// 	accum += byte;
-	// 	len += 1;
-	// }
-
-	Vec::default()
+	println!("\n=> {:?}", result);
+	result.0
 }
 
 #[cfg(test)]
@@ -58,6 +54,14 @@ mod test {
 
 	#[test]
 	fn plateaus_test() {
-		_diff_compress(vec![54, 56, 48, 50, 52, 49, 62, 205, 255], 10);
+		/*  avrg   =     [53                        ][205][255]*/
+		/*  diffs  =     [ 1,  3, -5, -3, -1, -4,  9][  0][  0]*/
+		let values = vec![54, 56, 48, 50, 52, 49, 62, 205, 255];
+		let compressed = _diff_compress(values, 10);
+
+		assert_eq!(
+			compressed,
+			vec![vec![54, 56, 48, 50, 52, 49], vec![62], vec![205], vec![255]]
+		)
 	}
 }
